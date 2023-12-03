478774836b2622c2b80fb9d0dc21cb7ccddd053d
patel <parthpatel167@gmail.com>
2023-12-03 09:45:27 -0600

    create the remove butten eliment andd eventlistioner to it its working

--- script.js
+++ script.js
@@ -11,21 +11,28 @@ form.addEventListener('submit', function (e) {
 
   const meme = document.createElement('div');
   meme.classList.add('meme');
-
+  // code for meme image
   const images = document.createElement('img');
   images.src = imageInput;
   images.onload = () => {
     meme.appendChild(images);
-
+    // code for top text
     const topElement = document.createElement('div');
     topElement.classList.add('memetxt', 'top');
     topElement.innerText = toptextInput;
     meme.appendChild(topElement);
-
+    // code for bottom text
     const bootomElement = document.createElement('div');
     bootomElement.classList.add('memetxt', 'bottom');
     bootomElement.innerText = bottomtextInput;
     meme.appendChild(bootomElement);
+    // code for deleting the meme
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'Remove';
+    removeButton.addEventListener('click', () => {
+      meme.remove();
+    });
+    meme.appendChild(removeButton);
 
     result.appendChild(meme);
     form.reset();
